dd06d7452d81f0873033a02572db93c14dd57621
akshita bhasin <145154439+AKS380@users.noreply.github.com>
2024-02-07 11:27:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Login from "./login";
 import Signup from "./signup";
 import Form from "./form";
 import BreastCancerForm from "./test2";
-import CharityWebsite from "./charity";
+
 
 
 function App(){
@@ -21,7 +21,7 @@ function App(){
         <Route path='/signup'element ={<Signup/>}/>
         <Route path='/form'element ={<Form/>}/>
         <Route path='/test2'element = {<BreastCancerForm/>}/>
-        <Route path='/charity'element = {<CharityWebsite/>}/>
+        
 
         
 
